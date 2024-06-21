29e1bdb68b7e20e0e0fe6285220482927a5ab010
NoiceFrog <alexdasilvamassanatavares@gmail.com>
2024-06-21 15:18:02 +0200

    fixed main file

--- spinning_oct.c
+++ spinning_oct.c
@@ -5,7 +5,7 @@
 
 float A, B, C;
 
-float octWidth = 20;
+float cubeWidth = 20;
 int width = 160, height = 44;
 float zBuffer[160 * 44];
 char buffer[160 * 44];
@@ -16,49 +16,35 @@ float K1 = 40;
 
 float incrementSpeed = 0.6;
 
-typedef struct {
-  float x;
-  float y;
-  float z;
-} vec3;
-
-vec3 oct;
-
+float x, y, z;
 float ooz;
 int xp, yp;
 int idx;
 
 float calculateX(int i, int j, int k) {
-  return (
-    j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) + 
-    j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C)
-    );
+  return (j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
+          j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C));
 }
 
 float calculateY(int i, int j, int k) {
-  return (
-    j * cos(A) * cos(C) + k * sin(A) * cos(C) - 
-    j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) - 
-    i * cos(B) * sin(C)
-    );
+  return (j * cos(A) * cos(C) + k * sin(A) * cos(C) -
+          j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
+          i * cos(B) * sin(C));
 }
 
 float calculateZ(int i, int j, int k) {
-  return (
-    k * cos(A) * cos(B) - 
-    j * sin(A) * cos(B) + i * sin(B)
-    );
+  return (k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B));
 }
 
-void calculateForSurface(float octX, float octY, float octZ, int ch) {
-  oct.x = calculateX(octX, octY, octZ);
-  oct.y = calculateY(octX, octY, octZ);
-  oct.z = calculateZ(octX, octY, octZ) + distanceFromCam;
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
+  x = calculateX(cubeX, cubeY, cubeZ);
+  y = calculateY(cubeX, cubeY, cubeZ);
+  z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
 
-  ooz = 1 / oct.z;
+  ooz = 1 / z;
 
-  xp = (int)(width / 2 + horizontalOffset + K1 * ooz * oct.x * 2);
-  yp = (int)(height / 2 + K1 * ooz * oct.y);
+  xp = (int)(width / 2 + horizontalOffset + K1 * ooz * x * 2);
+  yp = (int)(height / 2 + K1 * ooz * y);
 
   idx = xp + yp * width;
   if (idx >= 0 && idx < width * height) {
@@ -74,34 +60,66 @@ int main() {
   while (1) {
     memset(buffer, backgroundASCIICode, width * height);
     memset(zBuffer, 0, width * height * 4);
-    
-    
-    // first oct
-    octWidth = 20;
-    horizontalOffset = -2 * octWidth;
-    
-    for (float octX = -octWidth; octX < octWidth; octX += incrementSpeed) {
-      for (float octY = -octWidth; octY < octWidth; octY += incrementSpeed) {
-        calculateForSurface(octX, octY, -octWidth, '@');// lots of this
-        calculateForSurface(octWidth, octY, octX, '$');
-        calculateForSurface(-octWidth, octY, -octX, '~');
-        calculateForSurface(-octX, octY, octWidth, '#');
-        calculateForSurface(octX, -octWidth, -octY, ';');//doesnt showup alot
-        calculateForSurface(octX, octWidth, octY, '+');
+
+    // first cube
+    cubeWidth = 20;
+    horizontalOffset = -2 * cubeWidth;
+
+    for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+      for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+           cubeY += incrementSpeed) {
+        calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+        calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+        calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+        calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+        calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+        calculateForSurface(cubeX, cubeWidth, cubeY, '+');
       }
     }
-    
+
+    // second cube
+    cubeWidth = 10;
+    horizontalOffset = 1 * cubeWidth;
+
+    for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+      for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+           cubeY += incrementSpeed) {
+        calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+        calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+        calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+        calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+        calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+        calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+      }
+    }
+
+    // third cube
+    cubeWidth = 5;
+    horizontalOffset = 8 * cubeWidth;
+
+    for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+      for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+           cubeY += incrementSpeed) {
+        calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+        calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+        calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+        calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+        calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+        calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+      }
+    }
+
     printf("\x1b[H");
     for (int k = 0; k < width * height; k++) {
       putchar(k % width ? buffer[k] : 10);
     }
 
-    A += 0.0;
-    B += 0.0;
-    C += 0.0;
-    
+    A += 0.05;
+    B += 0.05;
+    C += 0.01;
+
     usleep(8000 * 2);
   }
-  
+
   return 0;
 }
