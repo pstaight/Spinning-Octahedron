92144677394f2edb6986be274f4dacad7ff21fcf
NoiceFrog <alexdasilvamassanatavares@gmail.com>
2024-06-21 15:37:40 +0200

    :)

--- test/REAL-TEST.c
+++ test/REAL-TEST.c
@@ -21,20 +21,16 @@ vec3 p1;
 vec3 p2;
 vec3 p3;
 
-bool p1H;
-bool p2H;
-bool p3H;
-
-void isHighest(vec3 p1, bool p1H, vec3 p2, bool p2H, vec3 p3, bool p3H) {
+vec3 isHighest(vec3 p1, vec3 p2, vec3 p3) {
   if (p1.y > p2.y && p1.y > p3.y) {
     // the y value of the point "p1" is larger than every other point
-    p1H = true;
+    return p1;
   } else if (p2.y > p1.y && p2.y > p3.y) {
     // the y value of the point "p2" is larger than every other point
-    p2H = true;
+    return p2;
   } else if (p3.y > p1.y && p3.y > p2.y) {
     // the y value of the point "p3" is larger than every other point
-    p3H = true;
+    return p3;
   }
 }
 
@@ -43,6 +39,7 @@ int main() {
   while (1) {
     memset(buff, ' ', width * height);
     memset(zbuff, 0, width * height * 4);
+    isHighest(p1, p2, p3);
   }
   return 0;
 }
