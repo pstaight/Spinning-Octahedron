1ccb3640272c857a00da392b7133b378fc7fc4f9
Patrick <pstaight@gmail.com>
2024-06-26 05:01:15 -0700

    Made 6 rotating points Defined the 6 points on the octihadron. Made a new function "drawTriangle" and called it 8 times, once for each face. Adapted Alex's code for finding top most point in a triagle. Drew the points. todo: fill in the triangles.

--- spinning_oct.c
+++ spinning_oct.c
@@ -21,12 +21,6 @@ typedef struct {
   float x, y, z;
 } vec3;
 
-vec3 oct;
-
-float ooz;
-int xp, yp;
-int idx;
-
 float calculateX(int i, int j, int k) {
   return (j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
           j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C));
@@ -42,27 +36,67 @@ float calculateZ(int i, int j, int k) {
   return (k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B));
 }
 
-void calculateForSurface(float octX, float octY, float octZ, int ch) {
-  oct.x = calculateX(octX, octY, octZ);
-  oct.y = calculateY(octX, octY, octZ);
-  oct.z = calculateZ(octX, octY, octZ) + distanceFromCam;
-
-  ooz = 1 / oct.z;
-
-  xp = (int)(width / 2 + horizontalOffset + K1 * ooz * oct.x * 2);
-  yp = (int)(height / 2 + K1 * ooz * oct.y);
+void drawPoint(vec3 p1,char paint){
+  vec3 p1_rotated;
+  float ooz;
+  int xp,yp,idx;
+  p1_rotated.x = calculateX(p1.x, p1.y, p1.z);
+  p1_rotated.y = calculateY(p1.x, p1.y, p1.z);
+  p1_rotated.z = calculateZ(p1.x, p1.y, p1.z) + distanceFromCam;
+        
+  ooz = 1 / p1_rotated.z;
+  xp = (int)(p1_rotated.x + horizontalOffset + width / 2);
+  yp = (int)(p1_rotated.y + height / 2);
 
   idx = xp + yp * width;
   if (idx >= 0 && idx < width * height) {
     if (ooz > zBuffer[idx]) {
       zBuffer[idx] = ooz;
-      buffer[idx] = ch;
+      buffer[idx] = paint;
     }
   }
 }
 
+void drawTriangle(vec3 p1, vec3 p2, vec3 p3, char paint){
+  vec3 top, mid, bot, oct;
+    if (p1.y > p2.y && p1.y > p3.y) {
+    // the y value of the point "p1" is larger than every other point
+    top = p1;
+    if(p2.y > p3.y){mid=p2;bot=p3;}
+    else{mid=p3;bot=p2;}
+  } else if (p2.y > p1.y && p2.y > p3.y) {
+    // the y value of the point "p2" is larger than every other point
+    top = p2;
+    if(p1.y > p3.y){mid=p1;bot=p3;}
+    else{mid=p3;bot=p1;}
+  } else if (p3.y > p1.y && p3.y > p2.y) {
+    // the y value of the point "p3" is larger than every other point
+    top = p3;
+    if(p2.y > p1.y){mid=p2;bot=p1;}
+    else{mid=p1;bot=p2;}
+  }
+  //todo: fill in the triangle instead of just drawing its 3 points.
+  drawPoint(top,paint);
+  drawPoint(mid,paint);
+  drawPoint(bot,paint);
+}
+
 int main() {
   printf("\x1b[2J");
+  vec3 p1,p2,p3,p4,p5,p6;
+  octWidth = 20;
+  //top and bottom
+  p1.x = 0; p1.y = octWidth; p1.z = 0;
+  p6.x = 0; p6.y =-octWidth; p6.z = 0;
+
+  //left and right
+  p2.x = octWidth; p2.y = 0; p2.z = 0;
+  p4.x =-octWidth; p2.y = 0; p2.z = 0;
+
+  //front and back
+  p3.x = 0; p3.y = 0; p3.z = octWidth;
+  p5.x = 0; p5.y = 0; p5.z =-octWidth;
+
   while (1) {
     memset(buffer, backgroundASCIICode, width * height);
     memset(zBuffer, 0, width * height * 4);
@@ -70,26 +104,23 @@ int main() {
     // first oct
     octWidth = 20;
     horizontalOffset = -2 * octWidth;
-
-    for (float octX = -octWidth; octX < octWidth; octX += incrementSpeed) {
-      for (float octY = -octWidth; octY < octWidth; octY += incrementSpeed) {
-        calculateForSurface(octX, octY, -octWidth, '@'); // lots of this
-        calculateForSurface(octWidth, octY, octX, '$');
-        calculateForSurface(-octWidth, octY, -octX, '~');
-        calculateForSurface(-octX, octY, octWidth, '#');
-        calculateForSurface(octX, -octWidth, -octY, ';'); // doesnt showup alot
-        calculateForSurface(octX, octWidth, octY, '+');
-      }
-    }
+    drawTriangle(p1,p2,p3,'@');
+    drawTriangle(p1,p2,p4,'$');
+    drawTriangle(p1,p5,p4,'~');
+    drawTriangle(p1,p5,p2,'#');
+    drawTriangle(p6,p2,p3,';');
+    drawTriangle(p6,p2,p4,'+');
+    drawTriangle(p6,p5,p4,'M');
+    drawTriangle(p6,p5,p2,'|');
 
     printf("\x1b[H");
     for (int k = 0; k < width * height; k++) {
       putchar(k % width ? buffer[k] : 10);
     }
 
-    A += 0.1;
-    B += 0.0;
-    C += 0.0;
+    A += 0.02;
+    B += 0.03;
+    C += 0.001;
 
     usleep(8000 * 2);
   }
